0af479b2adbb38635ea6d50b0475042bd365fe20
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-09 18:41:29 -0300

    Ugliest order algorithm ever

--- src/main/java/algorithms/sorts/InsertionSort.java
+++ src/main/java/algorithms/sorts/InsertionSort.java
@@ -3,31 +3,48 @@ package algorithms.sorts;
 import java.util.List;
 
 public class InsertionSort {
+	public static final Integer ASC = 1;
+	public static final Integer DESC = -1;
 
 	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled) {
 		if (shuffled.isEmpty())
 			return shuffled;
 		T head = shuffled.remove(0);
 
-		return recursiveSort(head, sort(shuffled));
+		return recursiveSort(head, sort(shuffled), InsertionSort.ASC);
 	}
 
-	private <T extends Comparable<? super T>> List<T> recursiveSort(T element, List<T> shuffled) {
+	private <T extends Comparable<? super T>> List<T> recursiveSort(T element, List<T> shuffled, Integer order) {
 		if (shuffled.isEmpty()) {
 			shuffled.add(0, element);
 			return shuffled;
 		}
 
-		if (element.compareTo(shuffled.get(0)) <= 0) {
-			shuffled.add(0, element);
-			return shuffled;
+		if (order.equals(InsertionSort.DESC)) {
+			if (shuffled.get(0).compareTo(element) < 0) {
+				shuffled.add(0, element);
+				return shuffled;
+			}
+		} else if (order.equals(InsertionSort.ASC)) {
+			if (element.compareTo(shuffled.get(0)) < 0) {
+				shuffled.add(0, element);
+				return shuffled;
+			}
 		}
 		T head = shuffled.remove(0);
 
-		recursiveSort(element, shuffled);
+		recursiveSort(element, shuffled, order);
 
 		shuffled.add(0, head);
 		return shuffled;
+	}
+
+	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled, Integer order) {
+		if (shuffled.isEmpty())
+			return shuffled;
+		T head = shuffled.remove(0);
+
+		return recursiveSort(head, sort(shuffled, order), order);
 	}
 
 }
--- src/test/java/algorithms/sorts/InsertionSortSpec.java
+++ src/test/java/algorithms/sorts/InsertionSortSpec.java
@@ -16,6 +16,16 @@ public class InsertionSortSpec {
 		List<Integer> shuffled = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 2, 3, 1 }));
 		List<Integer> sorted = new ArrayList<Integer>(Arrays.asList(new Integer[] { 1, 2, 3, 4 }));
 
-		it(insertion.sort(shuffled)).shouldEqual(sorted);
+		it(insertion.sort(shuffled, InsertionSort.ASC)).shouldEqual(sorted);
+	}
+
+	@Test
+	public void sortOrderTest() {
+		InsertionSort insertion = new InsertionSort();
+
+		List<Integer> shuffled = new ArrayList<Integer>(Arrays.asList(new Integer[] { 3, 2, 4, 1 }));
+		List<Integer> sorted = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 3, 2, 1 }));
+
+		it(insertion.sort(shuffled, InsertionSort.DESC)).shouldEqual(sorted);
 	}
 }
