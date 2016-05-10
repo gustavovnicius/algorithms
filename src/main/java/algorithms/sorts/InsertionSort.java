0999d16c8b432c6795cac3f321aee3ad26072fd2
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-10 00:22:03 -0300

    Dropped the recursive approach (for now). The warnings are gone

--- src/main/java/algorithms/sorts/InsertionSort.java
+++ src/main/java/algorithms/sorts/InsertionSort.java
@@ -1,44 +1,26 @@
 package algorithms.sorts;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class InsertionSort {
 	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled) {
-		if (shuffled.isEmpty())
-			return shuffled;
-		T head = shuffled.remove(0);
-
-		return recursiveSort(head, sort(shuffled), Order.ASC);
+		return sort(shuffled, new Order<T>().ASC);
 	}
 
-	private <T extends Comparable<? super T>> List<T> recursiveSort(T element, List<T> shuffled, BiFunction order) {
-		if (shuffled.isEmpty()) {
-			shuffled.add(0, element);
-			return shuffled;
+	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled,
+			BiFunction<Comparable<? super T>, T, Boolean> order) {
+		int length = shuffled.size();
+		for (int j = 1; j < length; j++) {
+			T key = shuffled.get(j);
+			int i = j - 1;
+			while ((i >= 0) && order.apply(shuffled.get(i), key)) {
+				shuffled.set(i + 1, shuffled.get(i));
+				i--;
+			}
+			shuffled.set(i + 1, key);
 		}
-
-		if ((boolean) order.apply(shuffled.get(0), element)) {
-			shuffled.add(0, element);
-			return shuffled;
-		}
-
-		T head = shuffled.remove(0);
-
-		recursiveSort(element, shuffled, order);
-
-		shuffled.add(0, head);
 		return shuffled;
-	}
-
-	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled, BiFunction order) {
-		if (shuffled.isEmpty())
-			return shuffled;
-		T head = shuffled.remove(0);
-
-		return recursiveSort(head, sort(shuffled, order), order);
 	}
 
 }
--- src/main/java/algorithms/sorts/Order.java
+++ src/main/java/algorithms/sorts/Order.java
@@ -2,7 +2,11 @@ package algorithms.sorts;
 
 import java.util.function.BiFunction;
 
-public class Order<T> {	
-	public static BiFunction ASC = 	(a, b) -> {  return ((Comparable) a).compareTo(b) > 0; };
-	public static BiFunction DESC = (a, b) -> {  return ((Comparable) b).compareTo(a) > 0; };
+public class Order<T> {
+	public BiFunction<Comparable<? super T>, T, Boolean> ASC = (a, b) -> {
+		return a.compareTo(b) > 0;
+	};
+	public BiFunction<Comparable<? super T>, T, Boolean> DESC = (a, b) -> {
+		return a.compareTo(b) < 0;
+	};
 }
