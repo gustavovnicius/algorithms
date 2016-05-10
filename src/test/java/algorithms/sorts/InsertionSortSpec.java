7b35f0c95764d56b1379b85c9b561b4e2e09e2a2
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-10 20:58:56 -0300

    Refactorings

--- src/test/java/algorithms/sorts/InsertionSortSpec.java
+++ src/test/java/algorithms/sorts/InsertionSortSpec.java
@@ -6,26 +6,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class InsertionSortSpec {
-	@Test
-	public void sortTest() {
-		InsertionSort insertion = new InsertionSort();
+	private List<Integer> sorted;
+	private List<Integer> reversed;
 
-		List<Integer> shuffled = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 2, 3, 1 }));
-		List<Integer> sorted = new ArrayList<Integer>(Arrays.asList(new Integer[] { 1, 2, 3, 4 }));
-
-		it(insertion.sort(shuffled)).shouldEqual(sorted);
+	@Before
+	public void setup() {
+		sorted = new ArrayList<Integer>(Arrays.asList(new Integer[] { 1, 2, 3, 4 }));
+		reversed = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 3, 2, 1 }));
 	}
 
 	@Test
-	public void sortOrderTest() {
+	public void ascendingOrderByDefaultTest() {
 		InsertionSort insertion = new InsertionSort();
+		List<Integer> shuffled = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 2, 3, 1 }));
 
+		it(insertion.sort(shuffled)).shouldBeEqual(sorted);
+	}
+
+	@Test
+	public void sortWithExplicitOrderTest() {
+		InsertionSort insertion = new InsertionSort();
 		List<Integer> shuffled = new ArrayList<Integer>(Arrays.asList(new Integer[] { 3, 2, 4, 1 }));
-		List<Integer> sorted = new ArrayList<Integer>(Arrays.asList(new Integer[] { 4, 3, 2, 1 }));
 
-		it(insertion.sort(shuffled, new Order<Integer>().DESC)).shouldEqual(sorted);
+		it(insertion.sort(shuffled, new Order<Integer>().DESC)).shouldBeEqual(reversed);
 	}
 }
