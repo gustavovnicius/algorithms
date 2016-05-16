60a986a402381682bb7eac648b9197dec4874ceb
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-05-15 21:28:18 -0300

    Removing unnecessary things

--- src/main/java/algorithms/sorts/HeapSort.java
+++ src/main/java/algorithms/sorts/HeapSort.java
@@ -1,11 +1,8 @@
 package algorithms.sorts;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class HeapSort {
 	public <T extends Comparable<? super T>> List<T> sort(List<T> shuffled) {
@@ -22,7 +19,7 @@ public class HeapSort {
 		return sorted;
 	}
 
-	public static class MaxHeap<T extends Comparable<? super T>> implements List<T> {
+	public static class MaxHeap<T extends Comparable<? super T>> {
 		private List<T> heap;
 
 		public MaxHeap() {
@@ -62,139 +59,6 @@ public class HeapSort {
 			System.out.println(this.heap);
 			return this.heap;
 		}
-
-		@Override
-		public int size() {
-			return this.heap.size();
-		}
-
-		@Override
-		public boolean isEmpty() {
-			return this.heap.isEmpty();
-		}
-
-		@Override
-		public boolean contains(Object o) {
-			return this.heap.contains(o);
-		}
-
-		@Override
-		public Iterator<T> iterator() {
-			return this.heap.iterator();
-		}
-
-		@Override
-		public Object[] toArray() {
-			return this.heap.toArray();
-		}
-
-		@Override
-		public <T> T[] toArray(T[] a) {
-			return this.heap.toArray(a);
-		}
-
-		@Override
-		public boolean add(T e) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean remove(Object o) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean containsAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean addAll(Collection<? extends T> c) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean addAll(int index, Collection<? extends T> c) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean removeAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public boolean retainAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
-		public void clear() {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public T get(int index) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public T set(int index, T element) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public void add(int index, T element) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public T remove(int index) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int indexOf(Object o) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public int lastIndexOf(Object o) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public ListIterator<T> listIterator() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ListIterator<T> listIterator(int index) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public List<T> subList(int fromIndex, int toIndex) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
 	}
 
 }
